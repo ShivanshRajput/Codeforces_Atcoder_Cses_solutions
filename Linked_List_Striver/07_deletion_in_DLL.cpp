6a21173ff7467db9f8a7fb8720e1f378b782a4d2
ShivanshRajput <shivansh.2022ug1003@iiitranchi.ac.in>
2024-05-19 21:35:21 +0530

    new file:   Linked_List_Striver/.cph/.08_insertion_in_DLL.cpp_d164be07eedee421bad453cdf87495a4.prob 	modified:   Linked_List_Striver/07_deletion_in_DLL.cpp 	modified:   Linked_List_Striver/07_deletion_in_DLL.exe 	new file:   Linked_List_Striver/08_insertion_in_DLL.cpp 	new file:   Linked_List_Striver/08_insertion_in_DLL.exe

--- Linked_List_Striver/07_deletion_in_DLL.cpp
+++ Linked_List_Striver/07_deletion_in_DLL.cpp
@@ -52,16 +52,54 @@ Node * deleteHead(Node* head){
 }   
 
 Node * deleteTail(Node* head){
+    if(head == nullptr or head->next==nullptr) return nullptr;
     Node * move = head;
-    
+    while(move->next != nullptr){
+        move = move->next;
+    }
+    Node * newtail = move->prev;
+    newtail->next = nullptr;
+    move->prev = nullptr;
+    delete move;
+    return head;
 }
+
+Node * deleteKthNode(Node * head , int k){
+    if(head==nullptr) return nullptr;
+    int cnt =0;
+    Node * kNode = head;
+    while(kNode != nullptr){
+        cnt++;
+        if(cnt==k) break;
+        kNode = kNode->next;
+    }
+    if(kNode==nullptr) return head;
+    if(kNode->prev==nullptr and kNode->next==nullptr){
+        return nullptr;
+    }
+    if(kNode->prev == nullptr) return deleteHead(head);
+    if(kNode->next == nullptr) return deleteTail(head);
+    Node * left = kNode->prev;
+    Node * right = kNode->next;
+    left->next = right;
+    right->prev = left;
+    delete(kNode);
+    return head;
+}
+
 
 int main(){
     vector<int>arr = {10,20,30,40,50,60,70,80};
     Node * head = arrToDLL(arr);
     head->printList();
 
-    head = deleteHead(head);
+    // head = deleteHead(head);
+    // head->printList();
+
+    // head = deleteTail(head);
+    // head->printList();
+
+    head = deleteKthNode(head,4 );
     head->printList();
 
 }
---
+++ Linked_List_Striver/08_insertion_in_DLL.cpp
@@ -0,0 +1,110 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+    public:
+        Node * prev;
+        int data;
+        Node * next;
+    public:
+    // constructor functions
+    Node(int data){
+        this->prev = nullptr;
+        this->data = data;
+        this->next = nullptr;
+    }
+    Node(Node *prev , int data , Node * next){
+        this->prev = prev;
+        this->data = data;
+        this->next = next;
+    }
+    // printing functions
+    void printList(){
+        Node *move = this;
+        while(move != nullptr){
+            cout<<move->data<<" ";
+            move = move ->next;
+        }
+        cout<<endl;
+    }
+};
+
+Node * arrToDLL(vector<int>arr){
+    Node * head = new Node(arr[0]);
+    Node * last = head;
+    for(int i=1;i<arr.size();i++){
+        Node * temp = new Node(last , arr[i] , nullptr);
+        last->next=temp;
+        last = temp;
+    }
+    return head;
+}
+
+// insertion before head
+Node * insertAtHead(Node * head , int data){
+    if(head == nullptr) return new Node(data);
+    Node * newnode = new Node(data);
+    newnode->next = head;
+    head->prev = newnode;
+    return newnode;
+}
+
+// insertion before tail
+Node * insertAtTail(Node * head , int data){
+    if(head->next==nullptr) return insertAtHead(head,data);
+    Node * move = head;
+    while(move->next != nullptr){
+        move = move->next;
+    }
+    Node * newnode = new Node(data);
+    newnode->prev = move->prev;
+    move->prev = newnode;
+    newnode->next = move;
+    newnode->prev->next = newnode;
+    return head;
+}
+
+// insert before kth node
+Node * insertAtKthNode(Node * head , int data , int k){
+    if(k==1){
+        return insertAtHead(head , data);
+    }
+    if(head == nullptr) return nullptr;
+    Node * move = head;
+    int cnt =0;
+    while(move!=nullptr){
+        cnt++;
+        if(cnt==k){
+            Node * newnode = new Node(data);
+            newnode->prev = move->prev;
+            move->prev = newnode;
+            newnode->next = move;
+            newnode->prev->next = newnode;
+            break;
+        }
+        move = move->next;
+    }
+    return head;
+}
+
+
+
+int main(){
+    vector<int> arr = {1,2,3,4,5,6};
+    Node * head = arrToDLL(arr);
+    head->printList();
+
+    // // insertion before head;
+    // head = insertAtHead(head , 99);
+    // head->printList();
+
+    // // insertion before tail;
+    // head = insertAtTail(head , 99);
+    // head->printList();
+    
+    // insert before kth element;
+    head = insertAtKthNode(head , 55 , );
+    head->printList();
+
+}
